021d88807736c8ac0def45bd700ad98fc94a951e
Seko461 <sekopierre@gmail.com>
2024-03-15 10:45:37 +0100

    add vuejs and initial milestone1

--- JS/app.js
+++ JS/app.js
@@ -3,7 +3,42 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+
+
+            contacts: [
+                {
+                    image: './assets/img/avatar_1.jpg',
+                    name: 'Michele'
+                },
+                {
+                    image: './assets/avatar_2.jpg',
+                    name: 'Fabio'
+                },
+                {
+                    image: './assets/avatar_3.jpg',
+                    name: 'Samuele'
+                },
+                {
+                    image: './assets/avatar_4.jpg',
+                    name: 'Alessandro B.'
+                },
+                {
+                    image: './assets/avatar_5.jpg',
+                    name: 'Alessandro L.'
+                },
+                {
+                    image: './assets/avatar_6.jpg',
+                    name: 'Angela'
+                },
+                {
+                    image: './assets/avatar_7.jpg',
+                    name: 'Giuseppe'
+                },
+                {
+                    image: './assets/avatar_8.jpg',
+                    name: 'Francesco'
+                }
+            ],
         }
     }
 }).mount('#app')
